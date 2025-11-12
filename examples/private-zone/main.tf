099b3a0986ee1ff0acf3453a432a2ba1b56003d9
Saranya3 <ssranya@google.com>
2025-11-12 04:06:03 +0000

    feat: Making TF Blueprint ADC compliant and adding tests

--- examples/private-zone/main.tf
+++ examples/private-zone/main.tf
@@ -15,8 +15,8 @@
  */
 
 module "dns-private-zone" {
-  source  = "terraform-google-modules/cloud-dns/google"
-  version = "~> 6.0"
+  source = "../.."
+  # [restore-marker]   version = "~> 0.1.7"
 
   project_id = var.project_id
   type       = "private"
@@ -68,4 +68,10 @@ module "dns-private-zone" {
       ]
     },
   ]
+
+  iam_choice   = "iam_policy"
+  managed_zone = "zone1"
+  role         = "roles/viewer"
+  member       = "user:jane@google.com"
+  members      = []
 }
