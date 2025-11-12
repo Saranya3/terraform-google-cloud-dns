51ffc02ae8d9d454eba52c1b7ff229ace091354d
Saranya3 <ssranya@google.com>
2025-11-12 04:06:03 +0000

    feat: Making TF Blueprint ADC compliant and adding tests

--- examples/dns_response_policy/main.tf
+++ examples/dns_response_policy/main.tf
@@ -15,8 +15,8 @@
  */
 
 module "dns_response_policy" {
-  source  = "terraform-google-modules/cloud-dns/google//modules/dns_response_policy"
-  version = "~> 6.0"
+  source = "../../modules/dns_response_policy"
+  # [restore-marker]   version = "~> 0.0.6"
 
   project_id         = var.project_id
   policy_name        = "dns-response-policy-test"
